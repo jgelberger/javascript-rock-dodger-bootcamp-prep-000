90081161666e2e98cfd1c0a7d0cb9eb55be16d4d
Joshua Gelberger <joshua_gelberger@alumni.brown.edu>
2018-09-19 02:56:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -163,15 +163,15 @@ function moveDodgerRight() {
    * This function should move DODGER to the right
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
-  var left = positionToInteger(DODGER.style.left);
+  var right = positionToInteger(DODGER.style.left);
  
-  function drop() {
-    DODGER.style.left = `${left - 4}px`;
-    if (left > 0) {
-      window.requestAnimationFrame(drop);
+  function drip() {
+    DODGER.style.left = `${right + 4}px`;
+    if (right < 360) {
+      window.requestAnimationFrame(drip);
     }
   }
-  window.requestAnimationFrame(drop);
+  window.requestAnimationFrame(drip);
 }
 }
 
